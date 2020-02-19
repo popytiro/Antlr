ec5a4e43aab40766744dbda4e6b095f475db37db
Yuki Fukasawa <fukasawa.yuki.fp5@is.naist.jp>
2020-02-19 16:53:58 +0900

    Researchフォルダを追加した

---
+++ Research/src/ast_analyze_executor.py
@@ -0,0 +1,31 @@
+import logging.config
+from ast.ast_processor import AstProcessor
+from ast.basic_info_listener import BasicInfoListener
+from pathlib import Path
+# import csv
+# import pprint
+
+# def code_search_net(dic, ast_process, post_process, frm=0, to=10000):
+#     java_files = Path("C:/Users/yuki-fu/Desktop/先行研究の再現/dataset/").glob("**/*.java")
+#     for file in java_files:
+
+
+if __name__ == '__main__':
+    # logging_setting_path = '../resources/logging/utiltools_log.conf'
+    # logging.config.fileConfig(logging_setting_path)
+    # logger = logging.getLogger(__file__)
+    c = 0
+    java_files = Path("C:/Users/yuki-fu/Desktop/先行研究の再現/dataset/").glob("**/*.java")
+    for file in java_files:
+        if c == 2:
+           exit()
+        c+=1
+
+        #target_file_path = 'C:/temp/解析対象のファイル.java'
+        target_file_path = file
+
+        # ★ポイント１
+        ast_info = AstProcessor(BasicInfoListener()).execute(target_file_path)
+        
+
+        # print(ast_info)
