47d4449d7664a7bd3b6d73041ece69f2e680ab15
RYOSUKE <kurachi.ryosuke.kp0@is.naist.jp>
2020-03-03 11:00:16 +0900

    divide multiple called method

--- mapCalledMethod/ast_analyze_executor.py
+++ mapCalledMethod/ast_analyze_executor.py
@@ -5,6 +5,7 @@ from ast.basic_info_listener import BasicInfoListener
 
 if __name__ == '__main__':
     target_file_path = '/Users/ryosuke/Desktop/cassandra/AbstractAllocator.java'
+    target_file_path = '/Users/ryosuke/Desktop/cassandra/AbstractBTreePartition.java'
 
     # ★ポイント１
     ast_info = AstProcessor(None, BasicInfoListener()).execute(target_file_path)
