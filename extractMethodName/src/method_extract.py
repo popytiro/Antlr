ee1ff69385a7646d81c946a9b9b8135d1efb2828
Yuki Fukasawa <fukasawa.yuki.fp5@is.naist.jp>
2020-04-08 14:59:59 +0900

    コメント消した

--- extractMethodName/src/method_extract.py
+++ extractMethodName/src/method_extract.py
@@ -1,11 +1,12 @@
 s1 = 'searchIterator(ColumnFilter.selection(columns()),false).next(clustering)'
 # s2 = 'current.staticRow.filter(columns,partitionDeletion,setActiveDeletionToRow,metadata)'
+test = 'buffer.remaining()'
 
 def extract_methods(text):
     res = []
     k = text.split('(')
     for e in k:
-        print('e：' + e)
+        # print('e：' + e)
         index = e.find('.')
         if index > 0:
             res.append(e[index+1:])
@@ -16,5 +17,6 @@ def extract_methods(text):
             res.remove(e)
     return res
 
-print(extract_methods(s1))
+# print(extract_methods(s1))
 # print(extract_methods(s2))
+print(extract_methods(test))
